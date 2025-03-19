39cde97290114899482d0252e534f952cebcbc55
Precious13ui (Daniel) <70605806+pui4@users.noreply.github.com>
2025-03-19 20:40:25 +0000

    crosshair

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,15 +1,20 @@
 extends Control
 
+@onready var panel = $"Panel"
 @onready var health_bar = $"Panel/Health Bar"
 @onready var health_txt = $"Panel/Health Bar/Health"
 
 @onready var icons = $"Panel/Icons"
 
 var old_equip : int = 0
+var old_y : float
+
+func _ready() -> void:
+    old_y = panel.position.y
 
 func _process(_delta: float) -> void:
     if Lib.player != null:
-        position.y = (-Lib.player.get_node("Head/hand").position.y - 0.368) * 175
+        panel.position.y = (-Lib.player.get_node("Head/hand").position.y - 0.368) * 175 + old_y
     
     health_txt.text = "\n" + str(Lib.player_health)
     health_bar.value = Lib.player_health
