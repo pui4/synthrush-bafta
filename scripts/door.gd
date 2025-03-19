35365fc31e19624468da4d8806d61ce21631674a
Precious13ui (Daniel) <70605806+pui4@users.noreply.github.com>
2025-03-19 19:29:10 +0000

    added door sfx

--- scripts/door.gd
+++ scripts/door.gd
@@ -10,6 +10,11 @@ var height : float
 var is_moving_up : bool = false
 var orig_y : float
 
+var door_sfx : FmodEventEmitter3D
+var stop_sfx : FmodEventEmitter3D
+var stop_sfx_has_played : bool = true
+var door_sfx_has_played : bool = false
+
 func _func_godot_apply_properties(props: Dictionary) -> void:
     targetname = props["targetname"] as String
 
@@ -24,6 +29,14 @@ func _ready() -> void:
         
     orig_y = position.y
 
+    door_sfx = FmodEventEmitter3D.new()
+    door_sfx.event_guid = "{47f9c732-ebc5-46ad-aac3-6e1cb9b686c9}"
+    add_child(door_sfx)
+
+    stop_sfx = FmodEventEmitter3D.new()
+    stop_sfx.event_guid = "{4ea24e3f-d490-4e21-885c-a5cb2cca6f81}"
+    add_child(stop_sfx)
+
 func targetfunc() -> void:
     is_moving_up = true
 
@@ -33,13 +46,31 @@ func _process(_delta: float) -> void:
 
     if is_moving_up:
         if position.y - orig_y - height < -buffer:
+            if not door_sfx_has_played:
+                door_sfx.play()
+                door_sfx_has_played = true
+            stop_sfx_has_played = false
             position.y += speed
         else:
+            door_sfx.stop()
+            door_sfx_has_played = false
+            if not stop_sfx_has_played:
+                stop_sfx.play()
+                stop_sfx_has_played = true
             position.y = height + orig_y - buffer
             await get_tree().create_timer(1).timeout
             is_moving_up = false
     else:
         if position.y > orig_y:
+            if not door_sfx_has_played:
+                door_sfx.play()
+                door_sfx_has_played = true
+            stop_sfx_has_played = false
             position.y -= speed
         else:
+            door_sfx.stop()
+            door_sfx_has_played = false
+            if not stop_sfx_has_played:
+                stop_sfx.play()
+                stop_sfx_has_played = true
             position.y = orig_y
